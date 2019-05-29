b07b65454decdd5190b682a7b01ee55ed27426ce
GAJENDIRAN25 <50912289+GAJENDIRAN25@users.noreply.github.com>
2019-05-29 04:32:00 -0700

    Update palindrome_for_number.py

--- palindrome_for_number.py
+++ palindrome_for_number.py
@@ -1,12 +1,10 @@
 n=int(input())
 rev=0
 temp=n
-
-if(n<=1000):
-  while(n!=0):
-    rem=n%10
-    rev=rev*10+rem
-    n=n//10
+while(n!=0):
+   rem=n%10
+   rev=rev*10+rem
+   n=n//10
 if(rev==temp):
   print('yes')
 else:
