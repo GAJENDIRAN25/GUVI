3865283c3ea2a24991cf3de894990c6e0f6ac8ec
GAJENDIRAN25 <50912289+GAJENDIRAN25@users.noreply.github.com>
2019-05-29 10:19:40 -0700

    Update armsnum_bwinterval.py

--- armsnum_bwinterval.py
+++ armsnum_bwinterval.py
@@ -2,11 +2,11 @@ x,y=input().split()
 a=int(x)
 b=int(y)
 for i in range(a+1,b):
-sum=0
-temp=i
+  temp=i
+  sum=0
   while(i!=0):
-    i=i%10
-    sum+=i*i*i
+    c=i%10
+    sum=sum+c*c*c
     i=i//10
-    if(temp==sum):
-      print(sum, end=' ')
+  if(temp==sum):
+    print(sum, end=' ')
