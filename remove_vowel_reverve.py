e9d16d28aa959b6fa6c910fe91cf78f08619edb1
GAJENDIRAN25 <50912289+GAJENDIRAN25@users.noreply.github.com>
2019-07-07 12:09:34 -0700

    Update remove_vowel_reverve.py

--- remove_vowel_reverve.py
+++ remove_vowel_reverve.py
@@ -1,11 +1,13 @@
 x=['a','e','i','o','u','A','E','I','O','U']
 l=[]
+lenth=int(input())
 a=input()
-for i in a:
-  if i in x:
-    continue
-  else:
-    l.append(i)
+if(lenth<=len(a)):
+  for i in a:
+    if i in x:
+      continue
+    else:
+      l.append(i)
 for i in l[::-1]:
   print(i, end='')
   
