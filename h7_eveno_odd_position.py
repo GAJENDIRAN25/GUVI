f7c697eb257a5f055ac5b03558d9421158abc3b5
GAJENDIRAN25 <50912289+GAJENDIRAN25@users.noreply.github.com>
2019-07-08 11:10:22 -0700

    Update h7_eveno_odd_position.py

--- h7_eveno_odd_position.py
+++ h7_eveno_odd_position.py
@@ -2,7 +2,7 @@ a=int(input())
 b=list(map(int,input().split()))
 for i in enumerate(b):
   if(i[0]%2==0):
-    if(i[1]%2!==0):
+    if(i[1]%2!=0):
       print(i[1], end=' ')
   if(i[0]%2!=0):
     if(i[1]%2==0):
