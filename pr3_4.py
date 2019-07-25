c3f12c5003281e63061eb6161f3645e18d69cb97
GAJENDIRAN25 <50912289+GAJENDIRAN25@users.noreply.github.com>
2019-07-24 22:16:56 -0700

    Update pr3_4.py

--- pr3_4.py
+++ pr3_4.py
@@ -1,6 +1,14 @@
-n1 = int(input())                  
+n1 = int(input())
+a1= []
+e1= []
 for i in range(2**n1):
     b1 = bin(i)[2:]
     l = len(b1)
     b1 = str(0) * (n1 - l) + b1
-    print (b1)
+    a1.append(b1)
+for i in a1:
+    c = i.count("1")
+    d = (c,i)
+    e1.append(d)
+for i in sorted(e1):
+    print(i[1])
